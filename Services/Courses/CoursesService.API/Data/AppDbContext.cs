19bc569a04f137ec54b80c2a7e92cc1379dc5fdf
tomiy7 <radenko.nikolic01@gmail.com>
2026-08-15 20:32:18 +0200

    feat(courses): created db context for courses

---
+++ Services/Courses/CoursesService.API/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CoursesService.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoursesService.API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) 
+        : base(options) { }
+    
+    public DbSet<Course> Courses => Set<Course>();
+    public DbSet<Lesson> Lessons => Set<Lesson>();
+    public DbSet<Section> Sections => Set<Section>();
+    public DbSet<TaskItem> Tasks => Set<TaskItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>().HasIndex(c => c.Slug).IsUnique();
+    }
+}
