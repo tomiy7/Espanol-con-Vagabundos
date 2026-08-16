1cb3543b4a3c55daaf8410d523e18f20820c3f40
tomiy7 <radenko.nikolic01@gmail.com>
2026-08-16 12:59:26 +0200

    feat(courses): add repository for courses to access the database

---
+++ Services/Courses/CoursesService.API/Repositories/CourseRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoursesService.API.Data;
+using CoursesService.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoursesService.API.Repositories;
+
+public class CourseRepository : ICourseRepository
+{
+    private readonly AppDbContext _context;
+    
+    public CourseRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Course>> GetCoursesAsync() =>
+        await _context.Courses.ToListAsync();
+
+    public async Task<List<Course>> GetPublishedCoursesAsync() =>
+        await _context.Courses.Where(c => c.IsPublished).ToListAsync();
+
+    public async Task<Course?> GetBySlugAsync(string slug) =>
+        await _context.Courses.FirstOrDefaultAsync(c => c.Slug == slug && c.IsPublished);
+
+    public async Task<Course?> GetByIdWithLessonsAsync(Guid id) =>
+        await _context.Courses
+            .Include(c => c.Lessons.Where(l => l.IsPublished && l.IsVisible))
+            .FirstOrDefaultAsync(c => c.Id == id && c.IsPublished);
+
+    public async Task<Lesson?> GetLessonByIdAsync(Guid id) =>
+        await _context.Lessons
+            .Include(l => l.Sections.OrderBy(s => s.Order))
+            .Include(l => l.Tasks.OrderBy(t => t.Order))
+            .FirstOrDefaultAsync(l => l.Id == id && l.IsPublished && l.IsVisible);
+}
---
+++ Services/Courses/CoursesService.API/Repositories/ICourseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CoursesService.API.Entities;
+
+namespace CoursesService.API.Repositories;
+
+public interface ICourseRepository
+{
+    Task<List<Course>> GetCoursesAsync();
+    Task<List<Course>> GetPublishedCoursesAsync();
+    Task<Course?> GetBySlugAsync(string slug);
+    Task<Course?> GetByIdWithLessonsAsync(Guid id);
+    Task<Lesson?> GetLessonByIdAsync(Guid id);
+}
