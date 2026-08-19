c9e7a119de0c5e7951390993536bf4cd62ada3ef
tomiy7 <radenko.nikolic01@gmail.com>
2026-08-19 22:15:34 +0200

    feat(payments): add IPaymentRepository and document generator interfaces

---
+++ Services/Payments/PaymentsService.Application/Interfaces/IPaymentRepository.cs
@@ -0,0 +1,11 @@
+﻿using PaymentsService.Domain.Entities;
+using PaymentsService.Domain.Interfaces;
+
+namespace PaymentsService.Application.Interfaces;
+
+public interface IPaymentRepository : IAsyncRepository<Payment>
+{
+    Task<List<Payment>> GetPendingAsync();
+    Task<List<Payment>> GetByUserIdAsync(Guid userId);
+    Task<Payment?> GetActivePendingForUserAndCourseAsync(Guid userId, Guid courseId);
+}
